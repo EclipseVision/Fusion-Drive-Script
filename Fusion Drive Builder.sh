8b151935007a03d26c0dab7f7d2ac11f3f8311a8
Dustin Harle <eclipsevision@me.com>
2012-12-16 22:35:43 -0600

    Start
    This is a small start to the script.

--- Fusion Drive Builder.sh
+++ Fusion Drive Builder.sh
@@ -5,3 +5,14 @@
 #
 #  Created by Dustin Harle on 12/16/12.
 #
+
+
+#This will show the list Disk on your mac
+diskutil list;
+
+
+echo "\n";
+echo ":Take note of the disk size of your HDD and SSD that you would wish to build your fusion drive with.:\n\n";
+echo "This is a list of the Disk that are on your mac.\n";
+echo "Look at the size of all the drive.";
+echo "once you have found the drives you wish to build your fusion drive with look to the right of the SIZE column and you will see the IDENTIFIER column.";
